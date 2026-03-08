efcaa28e2f52dd7c2bccb68704678dd425704178
I-Mougios <i.mougios.tech@gmail.com>
2026-03-08 17:08:51 +0200

    refactor(log_utils.handlers) signaling intent about the attributes store in logging.Handler subclasses

--- src/pyutils/log_utils/handlers.py
+++ src/pyutils/log_utils/handlers.py
@@ -2,6 +2,7 @@
 import asyncio
 import logging
 import sys
+import typing
 
 from motor.motor_asyncio import AsyncIOMotorClient
 from pymongo.errors import ConnectionFailure, OperationFailure, PyMongoError
@@ -12,13 +13,13 @@ from configurations import mongo_host, mongo_password, mongo_port, mongo_user
 class AsyncHandler(logging.Handler):
     def __init__(self, queue_max_size: int = 1000):
         super().__init__()
-        self.logs_queue: asyncio.Queue = asyncio.Queue(queue_max_size)
+        self._logs_queue: asyncio.Queue = asyncio.Queue(queue_max_size)
 
     def emit(self, record: logging.LogRecord):
         try:
             formatted_record = self.format(record)
             try:
-                self.logs_queue.put_nowait(formatted_record)
+                self._logs_queue.put_nowait(formatted_record)
             except asyncio.QueueFull:
                 # Queue is full, drop to prevent blocking the main app
                 sys.stderr.write(f"Log queue full, record {formatted_record} ignore")
@@ -45,8 +46,8 @@ class AsyncMongoHandler(AsyncHandler):
         db = client[database_name]
         self.collection = db[collection_name]
         self.batch_size = batch_size
-        self.batch: list[logging.LogRecord] = []
-        self.log_listener: asyncio.Task | None = None
+        self._batch: list[typing.Mapping] = []
+        self._log_listener: asyncio.Task | None = None
         self._mongo_ready_event = asyncio.Event()
 
     async def _consumer_loop(self) -> None:
@@ -56,10 +57,10 @@ class AsyncMongoHandler(AsyncHandler):
             while True:
                 # This will raise asyncio.QueueShutDown when the queue
                 # is empty AFTER shutdown() is called in the stop() method.
-                record = await self.logs_queue.get()
+                record = await self._logs_queue.get()
 
-                self.batch.append(record)
-                if len(self.batch) >= self.batch_size:
+                self._batch.append(record)
+                if len(self._batch) >= self.batch_size:
                     await self._flush_batch()
 
         except (asyncio.QueueShutDown, EOFError):
@@ -69,21 +70,21 @@ class AsyncMongoHandler(AsyncHandler):
             await self._flush_batch()
 
     async def _flush_batch(self) -> None:
-        if not self.batch:
+        if not self._batch:
             return
 
         try:
-            await self.collection.insert_many(self.batch)
+            await self.collection.insert_many(self._batch)
         except PyMongoError as e:
             sys.stderr.write(f"[Logging] error insert batch of logs: {e}")
         finally:
-            for _ in range(len(self.batch)):
-                self.logs_queue.task_done()
-            self.batch.clear()
+            for _ in range(len(self._batch)):
+                self._logs_queue.task_done()
+            self._batch.clear()
 
     async def start(self) -> None:
         # Schedule the logs consumer in the event loop
-        self.log_listener = asyncio.create_task(self._consumer_loop())
+        self._log_listener = asyncio.create_task(self._consumer_loop())
 
         # Start the log consumer and reassure that is actually running
         await self._mongo_ready_event.wait()
@@ -94,14 +95,14 @@ class AsyncMongoHandler(AsyncHandler):
             sys.stderr.write(f"[Logging] Failed to connect to MongoDB server: {e}\n")
 
     async def stop(self):
-        self.logs_queue.shutdown()
+        self._logs_queue.shutdown()
 
-        await self.logs_queue.join()
+        await self._logs_queue.join()
 
-        if self.log_listener and not self.log_listener.done():
-            self.log_listener.cancel()
+        if self._log_listener and not self._log_listener.done():
+            self._log_listener.cancel()
             try:
-                await self.log_listener
+                await self._log_listener
             except asyncio.CancelledError:
                 pass
 
